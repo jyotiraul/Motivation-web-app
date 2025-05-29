3475f740b757eb47c526adebb256313cf35c796f
jyotiraul <jyotiraul74@gmail.com>
2025-05-29 12:57:51 +0530

    updated

--- infra/variables.tf
+++ infra/variables.tf
@@ -2,3 +2,8 @@ variable "docker_image" {
   description = "Docker image to deploy"
   type        = string
 }
+
+variable "key_name" {
+  description = "Name of your existing EC2 Key Pair"
+  type        = string
+}
