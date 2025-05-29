9420ae20f01946895f8ecce2e349b0ea6e80f513
jyotiraul <jyotiraul74@gmail.com>
2025-05-29 12:01:10 +0530

    changes done

--- infra/ec2_setup.sh
+++ infra/ec2_setup.sh
@@ -4,8 +4,4 @@ apt-get install -y docker.io
 systemctl start docker
 systemctl enable docker
 usermod -a -G docker ubuntu
-docker pull rauljyoti/motivation-web-app:latest
-docker stop motivation-web || true
-docker rm motivation-web || true
-docker run -d --name motivation-web -p 5000:5000 rauljyoti/motivation-web-app:latest
-
+docker run -d -p 5000:5000 rauljyoti/motivation-web-app:latest
