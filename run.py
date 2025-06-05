551401d8bfc125f50d671e47319c4590e5f47583
jyotiraul <jyotiraul74@gmail.com>
2025-06-05 11:59:19 +0530

    code added & updated

--- run.py
+++ run.py
@@ -3,4 +3,7 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+#    app.run(debug=True)
+app.run(host='0.0.0.0', port=5000, debug=True)
+
+# app.run(host='0.0.0.0', port=5000, debug=True) This makes the app listen on all IPs, not just localhost
